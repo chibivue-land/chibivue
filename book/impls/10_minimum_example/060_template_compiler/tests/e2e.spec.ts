6a75f47f93dde9c0d847874de446e93cdc6f9eca
2nofa11 <agsh0505@gmail.com>
2024-02-05 17:23:05 +0900

    chapter's e2e tests ( 10_minimum_example/060_template_compiler)

---
+++ book/impls/10_minimum_example/060_template_compiler/tests/e2e.spec.ts
@@ -0,0 +1,26 @@
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+
+import { createApp } from '../packages'
+
+let host: HTMLElement
+const initHost = () => {
+  host = document.createElement('div')
+  host.setAttribute('id', 'host')
+  document.body.appendChild(host)
+}
+beforeEach(() => initHost())
+afterEach(() => host.remove())
+
+describe('10_minimum_example/060_template_compiler', () => {
+  it('should render template option', () => {
+    const app = createApp({
+      template: `<b class="hello" style="color: red;">Hello World!!</b>`,
+    })
+    app.mount('#host')
+
+    expect(host.innerHTML).toBe(
+      // prettier-ignore
+      '<b class="hello" style="color: red;">Hello World!!</b>',
+    )
+  })
+})
