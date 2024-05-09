0d396f7137ce4f65ea33be68217ddca901aeb5b9
yordan.ramchev <yordan.ramchev@mentormate.com>
2024-05-09 09:15:02 +0300

    feat: documate (#272)

--- book/online-book/.vitepress/theme/index.ts
+++ book/online-book/.vitepress/theme/index.ts
@@ -1,9 +1,18 @@
+import { h } from 'vue'
 import { useData, inBrowser } from 'vitepress'
 import DefaultTheme from 'vitepress/theme-without-fonts'
+import Documate from '@documate/vue'
 import './main.css'
+import '@documate/vue/dist/style.css'
 
 export default {
   ...DefaultTheme,
+  Layout: h(DefaultTheme.Layout, null, {
+    'nav-bar-content-before': () =>
+      h(Documate, {
+        endpoint: 'https://test123.us.aircode.run/ask',
+      }),
+  }),
   setup() {
     const { lang } = useData()
     if (inBrowser) {
