69a235b1fbb043a71f0b9b198c35327754205045
Ubugeeei <ubuge1122@gmail.com>
2024-12-03 16:42:11 +0900

    configure new eslint config

--- packages/runtime-core/src/apiComputed.ts
+++ packages/runtime-core/src/apiComputed.ts
@@ -1,6 +1,6 @@
 import { computed as _computed } from '@chibivue/reactivity'
 
 export const computed = ((getter: any) => {
-  // @ts-ignore
+  // @ts-expect-error
   return _computed(getter)
 }) as typeof _computed
